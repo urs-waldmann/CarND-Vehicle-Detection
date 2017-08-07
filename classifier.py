5dfd0af5d11e8ad0bfcab85b2615033dc94992ce
Urs Waldmann <urs_waldmann@gmx.net>
2017-08-07 22:10:24 +0200

    Update classifier.py.
    Add grid search with different C parameter. Result ~92%.

--- classifier.py
+++ classifier.py
@@ -171,41 +171,22 @@ X_train, X_test, y_train, y_test = train_test_split(scaled_X, y, test_size=0.2, 
 print('Using', orient, 'orientations, ', pix_per_cell, 'pixels per cell and', cell_per_block,'cells per block.')
 print('Feature vector length:', len(X_train[0]), '.')
 
+# Use a SVC with GridSearch().
+parameters = {'C':[0.1, 1, 10]}
 # Use a linear SVC. 
 svc = LinearSVC()
-# Check the training time for the SVC.
-t=time.time()
-svc.fit(X_train, y_train)
-t2 = time.time()
-print(round(t2-t, 2), 'Seconds to train SVC...')
-# Check the score of the SVC.
-print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
-# Check the prediction time for a single sample.
-t=time.time()
-n_predict = 10
-print('My SVC predicts: ', svc.predict(X_test[0:n_predict]))
-print('For these',n_predict, 'labels: ', y_test[0:n_predict])
-t2 = time.time()
-print(round(t2-t, 5), 'Seconds to predict', n_predict,'labels with SVC')
-
-'''
-# Use a SVC with GridSearch(). 
-#parameters = {'kernel':('linear', 'rbf'), 'C':[0.1, 1, 10], 'gamma':[1, 100, 1000]}
-parameters = {'kernel':['linear'], 'C':[0.1, 1, 10], 'gamma':[1, 100, 1000]}
-svr = svm.SVC()
-clf = GridSearchCV(svr, parameters)
+clf = GridSearchCV(svc, parameters)
 # Check the training time for the SVC.
 t=time.time()
 clf.fit(X_train, y_train)
 t2 = time.time()
-print(round(t2-t, 2), ' seconds to train SVC.')
+print(round(t2-t, 2), 'Seconds to train SVC...')
 # Check the score of the SVC.
-print('Test Accuracy of SVC = ', round(svr.score(X_test, y_test), 4))
+print('Test Accuracy of SVC = ', round(clf.score(X_test, y_test), 4))
 # Check the prediction time for a single sample.
 t=time.time()
 n_predict = 10
-print('My SVC predicts: ', svr.predict(X_test[0:n_predict]))
+print('My SVC predicts: ', clf.predict(X_test[0:n_predict]))
 print('For these',n_predict, 'labels: ', y_test[0:n_predict])
 t2 = time.time()
-print(round(t2-t, 5), ' seconds to predict', n_predict,' labels with SVC')
-'''
+print(round(t2-t, 5), 'Seconds to predict', n_predict,'labels with SVC')
